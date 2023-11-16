6c8eee6f15ef40ac10ff2950feba5d6dfad30837
Tzu-Hua <thuang348@gatech.edu>
2023-11-16 07:31:45 -0500

    added backend connection and db connection

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,45 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate} from 'react-router-dom';
 import '../styles/Login.css';
 import logo from '../images/EVS FOR ALL.png';
+import { useState } from "react";
+import axios from "axios";
 
 function Login() {
+
+  //stores input field information, passes to backend
+  const [user, setUser] = useState({
+    username: null,
+    password: null
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  
+  //function that runs when clicked Login button, sends data back to database and see if valid login information
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:8800/logins", user);
+      console.log(response);
+      alert("Success");
+
+      //edit here to new page if successfully logins 99999
+      navigate("/");
+
+    } catch (err) {
+      console.log(err);
+      // Check if the error response has a status code of 409 which indicates username exists
+      alert("Incorrect login details");
+    }
+  };
+
+
+
+
     return (
       <div className="login-container">
         <div className="login-logo">
@@ -14,13 +50,13 @@ function Login() {
           <form>
             <div className="input-container">
               <label htmlFor="username">Username</label>
-              <input type="text" id="username" placeholder="JohnSmith" />
+              <input type="text" id="username" placeholder="Username" onChange={handleChange} name="username"/>
             </div>
             <div className="input-container">
               <label htmlFor="password">Password</label>
-              <input type="password" id="password" placeholder="••••••••" />
+              <input type="password" id="password" placeholder="Password" onChange={handleChange} name = "password"/>
             </div>
-            <button type="submit" className="login-button">Login</button>
+            <button type="submit" className="login-button" onClick={handleClick}>Login</button>
             <NavLink to="/" className="action-button">Back</NavLink>
           </form>
           <div className="login-links">
