e9e89b8f4140a93d63d497b335d1a905e1a55a43
justin910113 <103973588+justin910113@users.noreply.github.com>
2024-04-21 13:41:30 -0400

    Create Car.js to make each car displayed a responsive react bootstrap component

---
+++ frontend/src/pages/Car.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Card, Button, Accordion } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Make sure to import Bootstrap CSS
+
+const Car = ({ car, price }) => {
+    return (
+        <Card className="mb-3" style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={car.photo_url} alt={`${car.manufacturer_name} ${car.model}`} />
+            <Card.Body>
+                <Card.Title>{car.manufacturer_name} {car.model}</Card.Title>
+                <Card.Text>
+                    Year: {car.model_year}
+                </Card.Text>
+                <Accordion>
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>More Info</Accordion.Header>
+                        <Accordion.Body>
+                            Electric Range: {car.electric_range} miles<br />
+                            Fuel Type: {car.fuel_name}<br />
+                            Drivetrain: {car.drivetrain}<br />
+                            Seating Capacity: {car.seating_capacity}<br />
+                            Price: ${price}
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default Car;
